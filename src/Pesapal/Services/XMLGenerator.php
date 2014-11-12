0b8fdf82693bec3b4c431e5b9238044bfd3fc7b2
Chencha Jacob <jmokua@gmail.com>
2014-11-12 18:49:14 +0300

    xml to entities

--- src/Pesapal/Services/XMLGenerator.php
+++ src/Pesapal/Services/XMLGenerator.php
@@ -15,6 +15,7 @@ class XMLGenerator
 {
     function getXMLFromOrder(Order $order)
     {
-        return "<?xml version=\"1.0\" encoding=\"utf-8\"?><PesapalDirectOrderInfo xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" Amount=\"" . $order->amount . "\" Description=\"" . $order->desc . "\" Type=\"" . $order->type . "\" Reference=\"" . $order->reference . "\" FirstName=\"" . $order->firstname . "\" LastName=\"" . $order->lastname . "\" Email=\"" . $order->email . "\" PhoneNumber=\"" . $order->phonenumber . "\" xmlns=\"http://www.pesapal.com\" />";
+        $post_xml= "<?xml version=\"1.0\" encoding=\"utf-8\"?><PesapalDirectOrderInfo xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" Amount=\"" . $order->amount . "\" Description=\"" . $order->desc . "\" Type=\"" . $order->type . "\" Reference=\"" . $order->reference . "\" FirstName=\"" . $order->firstname . "\" LastName=\"" . $order->lastname . "\" Email=\"" . $order->email . "\" PhoneNumber=\"" . $order->phonenumber . "\" xmlns=\"http://www.pesapal.com\" />";
+        return htmlentities($post_xml);
     }
 } 
