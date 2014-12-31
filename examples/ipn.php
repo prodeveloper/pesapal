acd5b8a595af939ae2c5e277e90b60757b848492
Chencha Jacob <jmokua@gmail.com>
2014-12-31 11:31:00 +0300

    updated examples

--- examples/ipn.php
+++ examples/ipn.php
@@ -4,7 +4,7 @@ require_once "config.php";
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$ipn_data= new \Pesapal\Values\IPNData('20141121:432409396580','change','1508');
+$ipn_data= new \Pesapal\Values\IPNData('54a34da93590d','change','35478207-b54f-43d2-8508-1183e53b25df');
 $pesapal=\Pesapal\Pesapal::make($config);
 $pesapal->ipn($ipn_data);
 
